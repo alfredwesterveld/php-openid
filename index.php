ae07ba20f2ecbb6ab4405209f5519dd1b4bbe542
Alfred Westerveld <alfredwesterveld@gmail.com>
2011-01-27 22:10:56 +0100

    added gravatar profile picture

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 require 'openid.php';
+
+/**
+ * Get either a Gravatar URL or complete image tag for a specified email address.
+ *
+ * @param string $email The email address
+ * @param string $s Size in pixels, defaults to 80px [ 1 - 512 ]
+ * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+ * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+ * @param boole $img True to return a complete IMG tag False for just the URL
+ * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+ * @return String containing either just a URL or a complete image tag
+ * @source http://gravatar.com/site/implement/images/php/
+ */
+function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+	$url = 'http://www.gravatar.com/avatar/';
+	$url .= md5( strtolower( trim( $email ) ) );
+	$url .= "?s=$s&d=$d&r=$r";
+	if ( $img ) {
+		$url = '<img src="' . $url . '"';
+		foreach ( $atts as $key => $val )
+			$url .= ' ' . $key . '="' . $val . '"';
+		$url .= ' />';
+	}
+	return $url;
+}
+
 try {
     $openid = new LightOpenID;
     if(!$openid->mode) {
@@ -66,9 +92,12 @@ try {
         echo 'User has canceled authentication!';
     } else {
         echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
+        $attributes = $openid->getAttributes();
         echo '<pre>';
-        print_r($openid->getAttributes());
+        print_r($attributes);
         echo '</pre>';
+        echo '<p>Your email: ' . $attributes['contact/email'] . '</p>';
+        echo '<p>Your display picture from gravatar: ' . get_gravatar($attributes['contact/email'], 80, 'mm',  'g', true) . '</p>';
     }
 } catch(ErrorException $e) {
     echo $e->getMessage();
